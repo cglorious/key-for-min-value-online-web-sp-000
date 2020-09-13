2cb3c3e5e93c4b97fdab0658d3b9508849264880
Catherine Gloria <catherinegloria@gmail.com>
2020-09-13 23:20:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,33 +14,33 @@ def key_for_min_value(name_hash)
   min_key #[return key]
 end
 
-def working_key_for_min_value(name_hash)
-  array = name_hash.collect do |key, value|
-    value
-  end
+#def working_key_for_min_value(name_hash)
+#  array = name_hash.collect do |key, value|
+#    value
+#  end
 
-  array #[1,2,3]
+#  array #[1,2,3]
 
   #iterate through the array
-  array.collect do |num|
-    first_value = array[0]
-    sorted_array = []
+#  array.collect do |num|
+#    first_value = array[0]
+#    sorted_array = []
 
-    if first_value > num
-    elsif first_value < num
-      sorted_array << num
-    elsif
-      array[0] > num
+#    if first_value > num
+#    elsif first_value < num
+#      sorted_array << num
+#    elsif
+#      array[0] > num
 
-    else array[0] < num
+#    else array[0] < num
 
   #determine which value is the lowest
   #compare values while iterating
 
-  min_value = array.min { |a,b| a <=> b}
+#  min_value = array.min { |a,b| a <=> b}
 
-  min_key = name_hash.each do |key, value|
-    return key if value == min_value
-  end
-  min_key
-end
+#  min_key = name_hash.each do |key, value|
+#    return key if value == min_value
+#  end
+#  min_key
+#end
