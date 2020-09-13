c12fd32112ea21aeefc74acdde1d0977d9bae2d2
Catherine Gloria <catherinegloria@gmail.com>
2020-09-13 22:53:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+def off_limits_key_for_min_value(name_hash)
+  array = name_hash.collect do |key, value|
+    value
+  end
+  min_value = array.min { |a,b| a <=> b}
+
+  min_key = name_hash.each do |key, value|
+    return key if value == min_value
+  end
+  min_key
+end
+
 def key_for_min_value(name_hash)
   array = name_hash.collect do |key, value|
     value
