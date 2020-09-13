feb690995fda1b58655ecd5d09566693b22fbeae
Catherine Gloria <catherinegloria@gmail.com>
2020-09-13 23:19:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def off_limits_key_for_min_value(name_hash)
+def key_for_min_value(name_hash)
   array = name_hash.collect do |key, value|
     value
   end #[array of values]
